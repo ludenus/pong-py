18d2bde93e577b7a3272d8bd7ecddc72adc0f00b
Andrey Andreyev <x0153362@gmail.com>
2024-06-30 21:35:44 +0300

    upd

--- pong_py/main.py
+++ pong_py/main.py
@@ -1,23 +1,32 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, Response, jsonify
 
 app = Flask(__name__)
 
 def get_addr():
-    addr = ":8080"
-    from_env = os.getenv("PONG_LISTENING_ADDRESS")
-
-    if from_env:
-        addr = from_env
-
-    if len(os.sys.argv) > 1:
-        addr = os.sys.argv[1]
-
+    addr = os.getenv("PONG_LISTENING_ADDRESS", ":8080")
     return addr
+
+def load_config():
+    config_file = os.getenv("PONG_CONFIG_FILE", "config.yaml")
+    try:
+        with open(config_file, 'rb') as file:  # Read as bytes
+            content = file.read()
+    except FileNotFoundError:
+        content = None
+    return content, config_file
+
+config_content, config_file = load_config()
 
 @app.route('/ping/<param>', methods=['GET'])
 def ping(param):
     return jsonify({"pong": param})
+
+@app.route('/config', methods=['GET'])
+def get_config():
+    if config_content is None:
+        return jsonify({"error": f"Config file '{config_file}' not found."}), 404
+    return Response(config_content, mimetype='text/plain')
 
 if __name__ == '__main__':
     addr = get_addr()
